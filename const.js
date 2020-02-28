b8ee01d3981d7fa023dcda00aa576e9a2eafd744
nivleM-ed <melvinedward98@gmail.com>
2020-02-28 12:09:39 +0800

    Update const.js

--- const.js
+++ const.js
@@ -2,6 +2,9 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config.json')[env];
 
+config.user = 'hvbpycwqycclkj';
+config.password = '00da7250cf91e7529efa9db013ea09163d45692e2dd0f8031ccd75bd181d1775';
+
 module.exports = {
     CONST_page_limit: 10,
     dbPool: config
