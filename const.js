2eab64ce0f7d9438d0828451febc72f6d7971f6f
nivleM-ed <melvinedward98@gmail.com>
2020-02-28 12:23:11 +0800

    Update const.js

--- const.js
+++ const.js
@@ -4,6 +4,7 @@ const config = require('./config/config.json')[env];
 
 config.user = "hvbpycwqycclkj";
 config.password = "00da7250cf91e7529efa9db013ea09163d45692e2dd0f8031ccd75bd181d1775";
+config.database = "d1ha0feb33cd3r";
 
 module.exports = {
     CONST_page_limit: 10,
